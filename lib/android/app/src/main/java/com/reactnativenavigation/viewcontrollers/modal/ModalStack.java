0f96c65cad631dc0353ae9b814f98a7f37fbbcd1
Paweł Staś <pawelstas@wp.pl>
2020-04-08 13:55:34 +0200

    Revert "Stop rejecting on dismissAllModals if no modals are displayed (#5991)"
    This reverts commit 97542810e46d3a44cab9e81046426731ca703887.

--- lib/android/app/src/main/java/com/reactnativenavigation/viewcontrollers/modal/ModalStack.java
+++ lib/android/app/src/main/java/com/reactnativenavigation/viewcontrollers/modal/ModalStack.java
@@ -84,8 +84,10 @@ public class ModalStack {
 
     public void dismissAllModals(ViewController root, Options mergeOptions, CommandListener listener) {
         if (modals.isEmpty()) {
+            listener.onError("Nothing to dismiss");
             return;
         }
+
         String topModalId = peek().getId();
         String topModalName = peek().getCurrentComponentName();
         int modalsDismissed = size();
