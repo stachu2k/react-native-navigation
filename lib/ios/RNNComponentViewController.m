adf1d93c079353d65205a5c4118f173c944e7084
Paweł Staś <pawelstas@wp.pl>
2020-04-08 13:55:34 +0200

    Revert "Fix statusBar.visible option (#5992)"
    This reverts commit c0640a3925828d9077924063d0091e5791e4d2cc.

--- lib/ios/RNNComponentViewController.m
+++ lib/ios/RNNComponentViewController.m
@@ -76,6 +76,10 @@
 	[self.eventEmitter sendOnSearchBarCancelPressed:self.layoutInfo.componentId];
 }
 
+- (BOOL)prefersStatusBarHidden {
+	return [_presenter isStatusBarVisibility:self.navigationController resolvedOptions:self.resolveOptions];
+}
+
 - (UIStatusBarStyle)preferredStatusBarStyle {
 	return [_presenter getStatusBarStyle:[self resolveOptions]];
 }
